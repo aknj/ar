e31995807b0f91ef1d2c5443f4f9dfcf44f3aa07
aknj <aknjne@gmail.com>
2017-02-03 00:31:12 +0100

    add two trackbars for the threshold operation

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 using namespace cv;
 
-const int WIDTH = 320;
-const int HEIGHT = 240;
+const int WIDTH = 320 * 1.2;
+const int HEIGHT = 240 * 1.2;
 const int FPS = 5;
 
 
@@ -25,10 +25,8 @@ int main() {
     cap.set(CV_CAP_PROP_FPS, FPS);
 
     int i = 0;
-    Mat frame, denoised, thresholdImg, canny;
+    Mat frame, thresholdImg;
     namedWindow("input", 1);
-    // namedWindow("thresholded", 1);
-    // namedWindow("canny", 1);
     namedWindow("contours_prev", 1);
 
     //- reading an image from file
@@ -39,6 +37,13 @@ int main() {
         printf("Could not open or find the image");
         return -1;
     }
+
+    //- trackbars for changing the parameters of adaptiveThreshold
+    int t1 = 75;
+    createTrackbar("thr_blocksize", "contours_prev", &t1, 121);
+
+    int t2 = 10;
+    createTrackbar("thr_c", "contours_prev", &t2, 20);
 
     for(;;) {
         if(!cap.grab())
@@ -61,27 +66,31 @@ int main() {
         // GaussianBlur(blurred, blurred, Size(3, 3), 0, 0);
         // medianBlur(frame, blurred, 5);
 
+        
+        int thr_blocksize = t1 / 2 * 2 + 3;
+        int thr_c = t2 - 10;
+
         adaptiveThreshold(frame, thresholdImg, 255, 
                           CV_ADAPTIVE_THRESH_GAUSSIAN_C, 
-                          CV_THRESH_BINARY, 75, 1);
+                          CV_THRESH_BINARY, thr_blocksize, thr_c);
         
-        // bitwise_not(blurred, blurred);
 
-        vector<vector<Point > > allContours;
-        vector<vector<Point > > quadContours;
+        vector<vector<Point> > allContours;
+        vector<vector<Point> > contours;
         findContours(thresholdImg, allContours, CV_RETR_LIST, 
                      CV_CHAIN_APPROX_NONE);
 
         for(size_t i = 0; i < allContours.size(); i++) {
             int contourSize = allContours[i].size();
             if(contourSize > 4) {
-                quadContours.push_back(allContours[i]);
+                contours.push_back(allContours[i]);
             }
         }
 
         Mat contours_prev = Mat::zeros(frame.size(), CV_8UC3);
+        
 
-        drawContours(contours_prev, quadContours, -1, Scalar(255,0,0));
+        drawContours(contours_prev, contours, -1, Scalar(255,0,0));
 
         //- find candidates
 
